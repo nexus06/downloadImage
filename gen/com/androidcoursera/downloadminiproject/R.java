5a5aef9a6c9f7a5ec5df2e46a37dada947d37389
Luis at nexus <luis.pulido@zktechnology.eu>
2015-04-22 00:34:29 +0200

    created

--- gen/com/androidcoursera/downloadminiproject/R.java
+++ gen/com/androidcoursera/downloadminiproject/R.java
@@ -25,17 +25,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f070004;
-        public static final int container=0x7f070000;
-        public static final int headlines_fragment=0x7f070001;
-        public static final int progress_loading=0x7f070005;
-        public static final int textView1=0x7f070002;
-        public static final int url=0x7f070003;
+        public static final int button1=0x7f070002;
+        public static final int progress_loading=0x7f070003;
+        public static final int textView1=0x7f070000;
+        public static final int url=0x7f070001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int generic_image_activity=0x7f030001;
-        public static final int main_activity=0x7f030002;
+        public static final int generic_image_activity=0x7f030000;
+        public static final int main_activity=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
